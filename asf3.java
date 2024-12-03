d1de5d064eed94f4380d13b338f111d2cbc51c17
S MOHAMMAD ASHRAF <ashrafali31033@gmail.com>
2024-12-03 19:31:40 +0530

    Add files via upload

---
+++ asf3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class asf3 {
+    public static void main(String[]args ){
+        //conversion
+
+       
+        char c='a';
+        int b=c;
+        b+=1;
+        char v=(char)b;
+        System.out.println(v);
+
+      
+    }
+
+    
+}
